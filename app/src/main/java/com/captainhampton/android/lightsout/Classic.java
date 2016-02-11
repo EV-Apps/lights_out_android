4b15dc1c1c2358a3df061bf6da4f8eba3d21f9c8
Ersin Ertan <ersin_the_ertan@hotmail.com>
2016-02-10 23:30:43 -0500

    Update: refactored file structure, previous had 2 modules, one of which had it's source and resources code in the project root directory
    Changes in Classic.java were copied from the old directory to the app module's directory.
    
    Build tools version
    
    Remove: delete java/com/captainhampton/android/lightsout and the res directory
    
    Add: build.gradle ejml gradle dependency, no .jar is needed.
    gradlelw.bat for window users
    settings.gradle, which will tell the project which modules should be included

--- app/src/main/java/com/captainhampton/android/lightsout/Classic.java
+++ app/src/main/java/com/captainhampton/android/lightsout/Classic.java
@@ -53,37 +53,114 @@ public class Classic extends AppCompatActivity implements View.OnClickListener {
         setupVariables();
     }
 
-    private boolean isConfigSolvable5x5() {
-        // TODO
-        return true;
+    private boolean isConfigSolvable5x5(SimpleMatrix light_vector) {
+
+        // 5x5 configuration is solvable if the vector of lights are orthogonal to these two
+        // vectors n1 and n2 that correspond to vectors in the null space of the adjacency matrix
+        // of the game board. (https://www.math.ksu.edu/math551/math551a.f06/lights_out.pdf)
+        SimpleMatrix n1 = new SimpleMatrix(new double[][]
+                { {0,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,0,1,1,1,0} });
+        n1 = n1.transpose();
+        SimpleMatrix n2 = new SimpleMatrix(new double[][]
+                { {1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,1,0,1,0,1,1,0,1,0,1} });
+        n2 = n2.transpose();
+
+        SimpleMatrix val1 = light_vector.mult(n1);
+        SimpleMatrix val2 = light_vector.mult(n2);
+
+        // TODO: CHECK THIS
+        return ( val1.elementSum() == 0 && val2.elementSum() == 0 );
+    }
+
+    private int findFirstIdx(SimpleMatrix C) {
+        int idx = -1;
+        for (int n = 0; n < C.numRows(); n++) {
+            for (int m = 0; m < C.numCols(); m++) {
+                if ( C.get(n,m) == 1 ) {
+                    idx = C.getIndex(n,m);
+                    return idx;
+                }
+            }
+        }
+        return idx;
+    }
+
+    private SimpleMatrix getRow(SimpleMatrix M, int row_num, int col_offset) {
+        SimpleMatrix row = M.extractMatrix(row_num,row_num+1,col_offset,M.numCols());
+        return row;
+    }
+
+    private SimpleMatrix getCol(SimpleMatrix M, int col_num, int row_offset) {
+        SimpleMatrix col = M.extractMatrix(row_offset,M.numRows(),col_num,col_num+1);
+        return col;
+    }
+
+    private SimpleMatrix setRow(SimpleMatrix M, SimpleMatrix row, int row_num, int col_offset) {
+
+        double val;
+        //Log.i("row_before_var",String.valueOf(row));
+        for (int j = col_offset; j < row.numCols(); j++ ) {
+            val = row.get(0,j);
+            M.set(row_num,j,val);
+        }
+        //Log.i("row_after_var",String.valueOf(row));
+
+        return M;
     }
 
     private SimpleMatrix g2rref(SimpleMatrix A) {
 
         // TODO : In progress - function that computes the reduced row echeleon form of a matrix
         //        over the field GF(2).
+        // TODO : Investigate setRow function.
         int i = 0;
         int j = 0;
 
         //double c = Double.longBitsToDouble(
         //        Double.doubleToRawLongBits(a) ^ Double.doubleToRawLongBits(b));
 
-        while ( (i < NUM_ROWS) && (j < NUM_COLS) ) {
+        while ( (i < A.numRows()) && (j < A.numCols()) ) {
             SimpleMatrix C = A.extractMatrix(i,A.numRows(),j,j+1);
-            // Find value and index of largest element in the remainder of column j.
-            int k = 0;
 
-            for (int n = 0; n < C.numRows(); n++) {
-                for (int m = 0; m < C.numCols(); m++) {
-                    //Log.i("MYTAG",String.valueOf(C.get(n,m)));
-                    if ( C.get(n,m) == 1 ) {
-                        k = n;
-                        break;
-                    }
-                }
+            // Find value and index of largest element in the remainder of column j.
+            int k = findFirstIdx(C) + i;
+            Log.i("k_var",String.valueOf(k));
+
+            //SimpleMatrix row_i = A.extractMatrix(i,i+1,j,A.numCols());
+            //Log.i("i_var",String.valueOf(row_i));
+
+            // Swap i-th and k-th rows.
+            if (k >= 0) {
+                //SimpleMatrix row_i = A.extractMatrix(i,i+1,j,A.numCols());
+                //SimpleMatrix row_k = A.extractMatrix(k,k+1,j,A.numCols());
+                SimpleMatrix row_i = getRow(A,i,j);
+                SimpleMatrix row_k = getRow(A,k,j);
+
+                //A = setRow(A,row_i,k,j); /// Something is fucked up with the row swaps.
+                //A = setRow(A,row_k,i,j);
+
+                //Log.i("i_var",String.valueOf(row_i));
+                //Log.i("k_var",String.valueOf(row_k));
             }
-            k = k + i - 1;
-            Log.i("MYTAG",String.valueOf(k));
+
+            // Save the right hand side of the pivot row
+            //SimpleMatrix aijn = A.extractMatrix(i,i+1,j,A.numCols());
+            SimpleMatrix aijn = getRow(A,i,j);
+
+            // Column we're looking at
+            //SimpleMatrix col = A.extractMatrix(0,A.numRows(),j,j+1);
+            SimpleMatrix col = getCol(A,j,0);
+
+            // Never XOR the pivot row against itself
+            col.set(i, 0);
+
+            //Log.i("col",String.valueOf(col));
+            //Log.i("aijn",String.valueOf(aijn));
+
+            // This builds an matrix of bits to flip
+            SimpleMatrix flip = col.mult(aijn);
+
+            // XOR the right hand side of the pivot row with all the other rows
 
             i++;
             j++;
@@ -132,8 +209,7 @@ public class Classic extends AppCompatActivity implements View.OnClickListener {
         A = A.combine(0, A.numCols(), b.transpose());
         A = g2rref(A);
 
-
-
+        // TODO: Display to user once calculated:
 
 //
 //        b = calculateLightVector();
@@ -307,8 +383,8 @@ public class Classic extends AppCompatActivity implements View.OnClickListener {
         bHint = (Button)findViewById(R.id.bHint);
         bHint.setOnClickListener(this);
 
-        tvNumMoves = (TextView) findViewById(R.id.tvNumMoves);
-        tvLevelTime = (TextView) findViewById(R.id.tvLevelTime);
+        tvNumMoves = (TextView)findViewById(R.id.tvNumMoves);
+        tvLevelTime = (TextView)findViewById(R.id.tvLevelTime);
 
         for (int i = 0; i < NUM_ROWS; i++) {
             for (int j = 0; j < NUM_COLS; j++) {
